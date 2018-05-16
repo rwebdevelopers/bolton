aba179afec83816d4d03e3f4b9a0570627bcefb7
RickyDev <ricky.mandigma@gmail.com>
2018-05-17 01:23:38 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+     $('.multiple-items').slick({
+	 		dots: true,
+			infinite: true,
+			speed: 1000,
+			fade: true,
+			slidesToShow: 1,
+			adaptiveHeight: true,
+			autoplay: true,
+ 			autoplaySpeed: 2000,
+ 			arrows: false,
+
+		});
